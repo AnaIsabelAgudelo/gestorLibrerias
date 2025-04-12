a5cccbe8dfce60af1452bc75350993af9fc733aa
ANAISA123 <ana.agudelo95@correo.tdea.edu.co>
2025-04-11 20:31:11 -0500

    ADD Navbar

--- biblioteca-front/src/App.js
+++ biblioteca-front/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 
+// Estas vistas puedes crearlas después
+const Users = () => <div className="container mt-4"><h2>Usuarios</h2></div>;
+const Rentals = () => <div className="container mt-4"><h2>Alquileres</h2></div>;
+const Books = () => <Home />; // Puedes crear Books.js luego, ahora apunta a Home.js
+
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/rentals" element={<Rentals />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ biblioteca-front/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+      <Link className="navbar-brand" to="/">Biblioteca</Link>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav ms-auto">
+          <li className="nav-item">
+            <Link className="nav-link" to="/books">Libros</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/users">Usuarios</Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/rentals">Alquileres</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
